ec189bfdd0682fa94943e1e65624ef9f2ee831b5
Joe <joeschwehr@gmail.com>
2019-10-14 13:53:56 -0700

    Updates

---
+++ precache-manifest.01e083f94e8f24be6d27e5f1251a4ee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab2fa5efdf75c2cdcdf91f41241da9d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "36441528645890bce460",
+    "url": "/robofriends/static/css/main.785d4ea3.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "36441528645890bce460",
+    "url": "/robofriends/static/js/main.563b0a6b.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
